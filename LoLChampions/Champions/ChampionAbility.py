c936badf67b509b0f3f09923c06ac60b4abedb73
Marvin <mdtensuan_dev@protonmail.com>
2021-04-05 11:46:37 +0800

    Added ChampionAbility to Thresh notebook.

--- LoLChampions/Champions/ChampionAbility.py
+++ LoLChampions/Champions/ChampionAbility.py
@@ -3,7 +3,7 @@ from dataclasses import dataclass, field
 @dataclass
 class ChampionAbility:
     ability_name: str
-    base_damage: list = field(init=False, default=[0,0,0,0,0])
+    base_damage: list = field(init=False, default_factory=list)
     base_damage_type: str = field(init=False, default='')
     scale_damage: float = field(init=False, repr=False)
     scale_damage_attr: str = field(init=False, repr=False)
